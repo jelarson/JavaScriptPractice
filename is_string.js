a890ac4bdc52e219aca2989e5cd4bc55fbd07064
Jess Larson <54457674+jelarson@users.noreply.github.com>
2020-03-10 09:27:03 -0600

    checks is variable is a string

---
+++ is_string.js
@@ -0,0 +1,13 @@
+const age = 26;
+const name = 'Jesse';
+
+console.log('Is a string?', typeof age === 'string');
+console.log('Is a string?', typeof name === 'string');
+
+
+if (typeof name === 'string') {
+  console.log("this is a string")
+}
+if (typeof age !== 'string') {
+  console.log("this is not a string")
+}
