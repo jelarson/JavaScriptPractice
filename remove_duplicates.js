877e0e77027778d1468c648aec83c18b0dd5891f
Jess Larson <54457674+jelarson@users.noreply.github.com>
2020-03-20 17:10:25 -0600

    solution added using .some method

--- remove_duplicates.js
+++ remove_duplicates.js
@@ -7,13 +7,38 @@ function arrayDiff(a, b) {
     return a
   } else {
     return a.filter((item) => {
+      let result = true
       for (x of b) {
-        return x !== item
+        if (x === item) {
+          result = false
+        }
       }
+      return result
     })
   }
 }
 
-console.log(arrayDiff([1, 2, 2, 2, 2, 3, 4], [2]))
-console.log(arrayDiff([1, 2, 2, 2, 2, 3, 4], []))
-console.log(arrayDiff([11, 13, 20, 2, 12, -17, -19], [2, -19, 12, 20, 13])) // [11, -17]
+// console.log(arrayDiff([1, 2, 2, 3, 4], [2]))
+// console.log(arrayDiff([1, 2, 2, 3, 4], []))
+// console.log(arrayDiff([11, 13, 20, 2, 12, -17, -19], [2, -19, 12, 20, 13])) // [11, -17]
+
+
+// arr = [1, 2, 2, 3, 2, 4, 2, 5]
+
+// console.log([...new Set(arr)])
+
+
+function arrayDiff1(a, b) {
+  if (b.length === 0) {
+    return a
+  } else {
+    return a.filter((item) => {
+      return !b.some(bItem => bItem === item)
+    })
+  }
+}
+
+
+console.log(arrayDiff1([1, 2, 2, 3, 4], [2]))
+console.log(arrayDiff1([1, 2, 2, 3, 4], []))
+console.log(arrayDiff1([11, 13, 20, 2, 12, -17, -19], [2, -19, 12, 20, 13])) // [11, -17]
